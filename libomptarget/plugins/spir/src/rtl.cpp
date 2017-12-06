cc0e9c64bf962c4c8c336c8aae020e7bff8efb5d
Daniel Schürmann <daniel.schuermann@campus.tu-berlin.de>
2017-12-06 16:20:10 +0100

    Create command queue on device init. Use thread_limit as max global work size.

--- libomptarget/plugins/spir/src/rtl.cpp
+++ libomptarget/plugins/spir/src/rtl.cpp
@@ -51,7 +51,6 @@ public:
     // per device information
     std::vector<cl_device_id> deviceIDs;
     std::vector<int32_t> maxWorkGroups;
-    std::vector<int32_t> nativeVectorWidth;
     std::vector<cl_context> CTX;
     std::vector<cl_command_queue> Queues;
     std::vector<FuncOrGblEntryTy> FuncGblEntries;
@@ -65,7 +64,7 @@ public:
       clGetPlatformIDs(platformIdCount, platformIds.data(), nullptr);
 
       // check version and devices
-      for(cl_platform_id id : platformIds) {
+      for (cl_platform_id id : platformIds) {
         char buffer[128];
         clGetPlatformInfo(id, CL_PLATFORM_VERSION, 128, buffer, NULL);
         if(strncmp("OpenCL 2.0 AMD", buffer, 14)) {
@@ -79,29 +78,22 @@ public:
         DP("Found %d OpenCL devices\n", numDevices);
 
         maxWorkGroups.resize(numDevices);
-        nativeVectorWidth.resize(numDevices);
         CTX.resize(numDevices);
         Queues.resize(numDevices);
         FuncGblEntries.resize(numDevices);
         platformId = id;
 
         // get device specific information
-        for(int i = 0; i < numDevices; i++) {
+        for (int i = 0; i < numDevices; i++) {
           cl_device_id deviceId = deviceIDs[i];
           clGetDeviceInfo(deviceId, CL_DEVICE_MAX_COMPUTE_UNITS, 4, &maxWorkGroups[i], nullptr);
-          clGetDeviceInfo(deviceId, CL_DEVICE_NATIVE_VECTOR_WIDTH_FLOAT, 4, &nativeVectorWidth[i], nullptr);
-          size_t size;
-          clGetDeviceInfo(deviceId, CL_DEVICE_MAX_PARAMETER_SIZE, 8, &size, nullptr);
-          DP("max parameter size is %zd\n", size);
-          cl_ulong memsize;
-          clGetDeviceInfo(deviceId, CL_DEVICE_GLOBAL_MEM_SIZE, 8, &memsize, nullptr);
-          DP("global mem size is %zd\n", memsize);
-
           DP("max WGs is: %d\n", maxWorkGroups[i]);
-          DP("vector width is: %d\n", nativeVectorWidth[i]);
+
+#ifdef OMPTARGET_DEBUG
           cl_uint addressmode;
           clGetDeviceInfo(deviceId, CL_DEVICE_ADDRESS_BITS, 4, &addressmode, nullptr);
           DP("addressing mode is %d bit\n", addressmode);
+#endif
         }
         //return;
       }
@@ -117,24 +109,33 @@ extern "C" {
 #endif
 
 int32_t __tgt_rtl_is_valid_binary(__tgt_device_image *image) {
-  return true; //TODO
+  uint32_t magicWord = *(uint32_t *)image->ImageStart;
+  // compare magic word in little endian and big endian:
+  return magicWord == 0x07230203 || magicWord == 0x03022307;
 }
 
 int32_t __tgt_rtl_number_of_devices() { return DeviceInfo.numDevices; } //fixme
 
 int32_t __tgt_rtl_init_device(int32_t device_id) {
-  cl_int status;
 
+  cl_int status;
   DP("Initialize SPIR device\n");
 
   // create context
   cl_context_properties props[] = {CL_CONTEXT_PLATFORM, (cl_context_properties) DeviceInfo.platformId, 0};
   DeviceInfo.CTX[device_id] = clCreateContext(props, 1, &DeviceInfo.deviceIDs[device_id], nullptr, nullptr, &status);
-
-  if(status != CL_SUCCESS) {
+  if (status != CL_SUCCESS) {
     DP("OpenCL Error: Failed to create context: %d\n", status);
     return OFFLOAD_FAIL;
   }
+
+  DeviceInfo.Queues[device_id] = clCreateCommandQueueWithProperties(
+          DeviceInfo.CTX[device_id], DeviceInfo.deviceIDs[device_id], nullptr, &status);
+  if (status != 0) {
+    DP("OpenCL Error: Failed to create CommandQueue: %d\n", status);
+    return OFFLOAD_FAIL;
+  }
+
   return OFFLOAD_SUCCESS;
 }
 
@@ -169,22 +170,17 @@ __tgt_target_table *__tgt_rtl_load_binary(int32_t device_id,
   fclose(ftmp);
   DP("written to tmp\n")
   */
-  // create CommandQueue and Program
+
+  // create Program
   cl_int status;
-  DeviceInfo.Queues[device_id] = clCreateCommandQueueWithProperties(
-          DeviceInfo.CTX[device_id], DeviceInfo.deviceIDs[device_id], nullptr, &status);
-  if(status != 0) {
-    DP("OpenCL Error: Failed to create CommandQueue: %d\n", status);
-    return NULL;
-  } // should we create the command queue here or on kernel execution or on initialization?
   cl_program program = clCreateProgramWithIL(DeviceInfo.CTX[device_id], image->ImageStart, ImageSize, &status);
-  if(status != 0) {
+  if (status != 0) {
     DP("OpenCL Error: Failed to create program: %d\n", status);
     return NULL;
   }
 
   status = clBuildProgram(program, 0, nullptr, nullptr, nullptr, nullptr);
-  if(status != 0) {
+  if (status != 0) {
     DP("OpenCL Error: Failed to build program: %d\n", status);
     return NULL;
   } else {
@@ -214,9 +210,9 @@ __tgt_target_table *__tgt_rtl_load_binary(int32_t device_id,
 }
 
 void *__tgt_rtl_data_alloc(int32_t device_id, int64_t size, void *hst_ptr) {
-  cl_int status=0;
+  cl_int status;
   cl_mem mem = clCreateBuffer(DeviceInfo.CTX[device_id], CL_MEM_READ_WRITE, size, NULL, &status);
-  if(status != CL_SUCCESS) {
+  if (status != CL_SUCCESS) {
     DP("OpenCL Error: Failed to allocate memory: %d\n", status);
     return NULL;
   }
@@ -227,8 +223,7 @@ int32_t __tgt_rtl_data_submit(int32_t device_id, void *tgt_ptr, void *hst_ptr,
                               int64_t size) {
   cl_int status = clEnqueueWriteBuffer(DeviceInfo.Queues[device_id], (cl_mem) tgt_ptr, CL_TRUE, 0, size, hst_ptr, 0,
                                        nullptr, nullptr);
-
-  if(status != CL_SUCCESS) {
+  if (status != CL_SUCCESS) {
     DP("OpenCL Error: Failed to write buffer: %d\n", status);
     return OFFLOAD_FAIL;
   }
@@ -239,7 +234,7 @@ int32_t __tgt_rtl_data_retrieve(int32_t device_id, void *hst_ptr, void *tgt_ptr,
                                 int64_t size) {
   cl_int status = clEnqueueReadBuffer(DeviceInfo.Queues[device_id], (cl_mem) tgt_ptr, CL_TRUE, 0, size, hst_ptr, 0,
                                        nullptr, nullptr);
-  if(status != CL_SUCCESS) {
+  if (status != CL_SUCCESS) {
     DP("OpenCL Error: Failed to read buffer: %d\n", status);
     return OFFLOAD_FAIL;
   }
@@ -258,63 +253,61 @@ int32_t __tgt_rtl_run_target_team_region(int32_t device_id, void *tgt_entry_ptr,
                                          void **tgt_args, ptrdiff_t *tgt_offsets, int32_t arg_num, int32_t team_num,
                                          int32_t thread_limit, uint64_t loop_tripcount /*not used*/) {
   cl_int status;
-  DP("device id is %d\n", device_id);
-  // debug...
   cl_kernel * kernel = static_cast<cl_kernel *> (tgt_entry_ptr);
+  // debug...
+#ifdef OMPTARGET_DEBUG
   char buffer[128];
   clGetKernelInfo(*kernel, CL_KERNEL_FUNCTION_NAME, 128, buffer, nullptr);
   cl_uint n;
   clGetKernelInfo(*kernel, CL_KERNEL_NUM_ARGS, sizeof(cl_uint), &n, nullptr);
   DP("number of kernel parameters: %d\n", n);
   DP("number of arguments: %d\n", arg_num);
-
+#endif
 
   // set kernel args
   std::vector<void *> ptrs(arg_num);
   for (int32_t i = 0; i < arg_num; ++i) {
-
     ptrs[i] = (void *)((intptr_t)tgt_args[i] + tgt_offsets[i]);
     status = clSetKernelArg(*kernel, i, /*sizeof(intptr_t) + tgt_offsets[i]*/ sizeof(cl_mem), &ptrs[i]);
-    if(status != CL_SUCCESS) {
-      DP("OpenCL Error: Failed to set kernel arg %d for %s: %d\n", i, buffer, status);
+    if (status != CL_SUCCESS) {
+      DP("OpenCL Error: Failed to set kernel arg %d: %d\n", i, status);
       return OFFLOAD_FAIL;
     } else {
       DP("OpenCL: Kernel Arg %d set successfully\n", i);
     }
   }
 
-  // calculate number of threads in each team:
+  size_t global_work_size;
   size_t local_work_size;
+  // calculate number of threads in each team:
   clGetKernelWorkGroupInfo(*kernel, DeviceInfo.deviceIDs[device_id], CL_KERNEL_PREFERRED_WORK_GROUP_SIZE_MULTIPLE,
                            sizeof(size_t), &local_work_size, nullptr);
-  DP("preferred workgroup size multiple is: %zd\n", local_work_size);
-  if(thread_limit && local_work_size > thread_limit) { // thread limit == 1 means unspecified for whatever reason
-    // we should not create teams bigger than thread_limit
-    local_work_size = thread_limit;
-  }
 
   // calculate total number of threads to execute the kernel:
-  size_t global_work_size;
-  if(team_num) { // num_teams() is specified
+  if (thread_limit && team_num)
+    global_work_size = std::min(global_work_size, (size_t)thread_limit);
+  else if (thread_limit)
+    global_work_size = thread_limit;
+  else if (team_num)
     global_work_size = local_work_size * team_num;
-  } else {
-    global_work_size = local_work_size * DeviceInfo.maxWorkGroups[device_id] * 8; // have sane defaults
-  }
+  else
+    global_work_size = local_work_size * DeviceInfo.maxWorkGroups[device_id] * 64; // have sane defaults
 
   // run kernel:
   DP("thread limit is %d, team num is %d\n", thread_limit, team_num);
   DP("global work size is %zd\n", global_work_size);
-  DP("local work size is %zd\n", local_work_size);
+  DP("local work size is: %zd\n", local_work_size);
+
   status = clEnqueueNDRangeKernel(
           DeviceInfo.Queues[device_id], *kernel, 1, nullptr, &global_work_size, &local_work_size, 0, nullptr, nullptr);
-  if(status != CL_SUCCESS) {
+  if (status != CL_SUCCESS) {
     DP("OpenCL Error: Failed to enqueue kernel: %d\n", status);
     return OFFLOAD_FAIL;
   }
 
   DP("OpenCL: Started executing kernel.\n");
   status = clFinish(DeviceInfo.Queues[device_id]);
-  if(status != CL_SUCCESS) {
+  if (status != CL_SUCCESS) {
     DP("OpenCL Error: Failed to execute kernel: %d\n", status);
   } else {
     DP("OpenCL: Successfully finished kernel execution.\n")
